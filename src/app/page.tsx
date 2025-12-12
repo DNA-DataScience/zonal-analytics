8857c267f27b372675b921d25d73e075756478fd
KindHumberUser <sagarranga@outlook.com>
2025-12-12 16:23:57 +0530

    Changed source

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ const HomePage: React.FC = () => {
   return (
     <>
       <Map />
-      <SourcesButton href="https://data.humdata.org/dataset/ourairports-ind" />
+      <SourcesButton href="https://suzlonenergy365-my.sharepoint.com/:w:/g/personal/45579_suzlon_com/IQDJIKnL2RipRbIV63siC7lZASGR1umFAFehwrGeP4ctWL4?e=B1iv8p" />
     </>
   );
 };
