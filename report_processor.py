9c757baa29b2e118bc2994feead4a33ab4ae4a04
KindHumberUser <sagarranga@outlook.com>
2026-01-08 17:36:46 +0530

    Added nearest airport detection if no airports in point

--- report_processor.py
+++ report_processor.py
@@ -24,6 +24,27 @@ REPORT_QUERY = text("""
                         AND ST_Intersects(z.geom3857, p.geom3857);                  
                     """)
 
+NEAREST_QUERY = text("""
+                     WITH p AS (
+                        SELECT 
+                        ST_SetSRID(ST_MakePoint(:lon, :lat), 4326) AS geom4326,
+                        ST_Transform(
+                            ST_SetSRID(ST_MakePoint(:lon, :lat), 4326),
+                            3857
+                        ) AS geom3857
+                    )
+                    SELECT z.zone, z.name, z.type, z.radio, z.elevation,
+                           ST_Distance(
+                               p.geom4326::geography,
+                               z.airport_point::geography
+                            ) AS distance_m
+                    FROM "GisDB".airport_layers z
+                    CROSS JOIN p
+                    ORDER BY
+                        z.geom3857 <-> p.geom3857
+                    LIMIT 1;    
+                     """)
+
 async def generate_report(lat: float, lng: float, elev: float = 0, db: AsyncSession = None):
     
     try:
@@ -36,7 +57,11 @@ async def generate_report(lat: float, lng: float, elev: float = 0, db: AsyncSess
         
     except Exception as e:
         print(f"Error retrieving zone data: {str(e)}")
-        return False
+        return HTTPException(status_code=500, detail=str(e))
+    
+    if not rows:
+        #TODO nearest airports
+        res = await nearest_airport(lat, lng, elev, db)
     
     report = []
     
@@ -84,3 +109,42 @@ def calc_min_height(elev:float, air_elev:float, distance_m:float):
 
     mh = min(air_elev + (45 + 0.05 * (distance_m - 4000)) - elev, 300)
     return mh
+
+async def nearest_airport(lat: float, lng: float, elev: float = 0, db: AsyncSession = None):
+    try:
+        result = await db.execute(NEAREST_QUERY, {
+            "lat": lat,
+            "lon": lng
+        })
+        
+        row = result.fetchall()
+        
+    except Exception as e:
+        return HTTPException(status_code=500, detail=str(e))
+    
+    report = []
+    r = row[0]
+    zone = "nearest"
+    name = r[1]
+    air_type = r[2]
+    radio = r[3]
+    air_elev = r[4]
+    distance_m = float(r[5])
+    feasibility = "Feasible"
+    note = "N/A"
+    min_height = "Not Required"
+    
+    report.append({
+            "zone": zone,
+            "name": name,
+            "type": air_type,
+            "radio": radio,
+            "airport_elevation": air_elev,
+            "min_height": min_height,
+            "note": note,
+            "feasibility": feasibility
+        })
+    
+    print(report)
+        
+    return JSONResponse(report)
